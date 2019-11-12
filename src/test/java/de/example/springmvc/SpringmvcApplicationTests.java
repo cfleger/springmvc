578803cd841abefea82f8b3c9e1708e085d5e9b3
Christian <gitkraken@fleger-mit-f.de>
2019-11-12 21:25:58 +0100

    added simple test

--- src/test/java/de/example/springmvc/SpringmvcApplicationTests.java
+++ src/test/java/de/example/springmvc/SpringmvcApplicationTests.java
@@ -1,13 +1,40 @@
 package de.example.springmvc;
 
+import de.example.springmvc.domain.Customer;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 
-@SpringBootTest
+import javax.annotation.PostConstruct;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 class SpringmvcApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@LocalServerPort
+	private int port;
+
+private RestTemplate restTemplate;
+
+	@PostConstruct
+	public void init() {
+		restTemplate = new RestTemplateBuilder().uriTemplateHandler(new DefaultUriBuilderFactory("http://localhost:"+port)).build();
 	}
+
+		@Test
+	void contextLoads() {
+			ResponseEntity<Customer> customerResponseEntity = restTemplate.getForEntity("/customers/5dc7dda28297ef37873e58cc", Customer.class);
+			assertThat(customerResponseEntity.getStatusCode().value(), equalTo(200));
+		}
 
 }
